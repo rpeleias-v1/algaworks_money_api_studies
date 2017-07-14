fbdf865a56d5b5adf825d8f9c991014d4ae7b378
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2017-07-14 16:09:15 -0300

    Evolução da API

---
+++ src/main/java/com/algaworks/algamoneyapi/resource/PessoaResource.java
@@ -0,0 +1,53 @@
+package com.algaworks.algamoneyapi.resource;
+
+import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
+import com.algaworks.algamoneyapi.model.Pessoa;
+import com.algaworks.algamoneyapi.repository.PessoaRepository;
+import com.algaworks.algamoneyapi.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.OK).body(pessoaSalva);
+    }
+
+    @GetMapping(value = "/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+        Pessoa pessoa = pessoaRepository.findOne(codigo);
+        return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        pessoaRepository.delete(codigo);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+        Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+        return ResponseEntity.ok(pessoaSalva);
+    }
+}
